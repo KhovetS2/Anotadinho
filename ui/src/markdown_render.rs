d99559b679ee4845cd6167638a459d0d76a876e6
KhovetS2 <isaque.elis.escola@gmail.com>
2026-08-06 01:15:39 -0300

    feat(063-067): PageView router, kanban block embed, calendar/table support

--- ui/src/markdown_render.rs
+++ ui/src/markdown_render.rs
@@ -1,6 +1,7 @@
 //! Renderizador Markdown → HTML usando pulldown-cmark.
 
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, Options, Parser, Tag, TagEnd, Event, CodeBlockKind};
+use pulldown_cmark::CowStr;
 
 /// Converte Markdown em HTML.
 pub fn render(markdown: &str) -> String {
@@ -11,7 +12,33 @@ pub fn render(markdown: &str) -> String {
     options.insert(Options::ENABLE_TASKLISTS);
 
     let parser = Parser::new_ext(markdown, options);
+    let mut in_kanban = false;
+
+    let events = parser.map(move |event| {
+        match event {
+            Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(ref lang))) => {
+                let lang_str = lang.as_ref();
+                if lang_str == "kanban" || lang_str == "calendar" || lang_str == "table" {
+                    in_kanban = true;
+                    Event::Html(CowStr::from(format!("<div class=\"embed-{}\" data-embed=\"{}\">", lang_str, lang_str)))
+                } else {
+                    let cls = if lang_str.is_empty() { String::new() } else { format!(" class=\"language-{}\"", lang_str) };
+                    Event::Html(CowStr::from(format!("<pre><code{}>", cls)))
+                }
+            }
+            Event::End(TagEnd::CodeBlock) => {
+                if in_kanban {
+                    in_kanban = false;
+                    Event::Html(CowStr::from("</div>"))
+                } else {
+                    Event::Html(CowStr::from("</code></pre>"))
+                }
+            }
+            other => other,
+        }
+    });
+
     let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
+    html::push_html(&mut html_output, events);
     html_output
 }
